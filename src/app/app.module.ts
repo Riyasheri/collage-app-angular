3016419fa2aa1a2957f1a6707c2839edc77a4a04
Riyasheri <Riyariyaaddon2022@gmail.com>
2022-07-06 22:42:30 +0530

    department

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FacultyComponent } from './faculty/faculty.component';
+import { DepartmentComponent } from './department/department.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FacultyComponent
+    FacultyComponent,
+    DepartmentComponent
   ],
   imports: [
     BrowserModule,
